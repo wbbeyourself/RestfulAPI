d0fee601318229023149b834fd265c9473569589
wbbeyourself <beyourselfwb@163.com>
2017-10-03 14:01:12 +0800

    add errorhandler  get_tasks

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 @time: 2017/10/3 13:19
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -24,14 +24,39 @@ tasks = [
 ]
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 @app.route('/')
 def index():
     return "Hello, World!"
 
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/todo/api/v1.0/tasks', methods=['GET', 'POST'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    if request.method == 'GET':
+        return jsonify({'tasks': tasks})
+    elif request.method == 'POST':
+        if not request.json or 'title' not in request.json:
+            abort(404)
+        task = {
+            'id': tasks[-1]['id'] + 1,
+            'title': request.json['title'],
+            'description': request.json.get('description', ''),
+            'done': False
+        }
+        tasks.append(task)
+        return jsonify({'task': task}), 201
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [t for t in tasks if t['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
 
 
 if __name__ == '__main__':
