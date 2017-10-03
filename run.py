da050e197dad81f9fee48981697a8c88eb88c48a
wbbeyourself <beyourselfwb@163.com>
2017-10-03 14:29:38 +0800

    自定义端口

--- run.py
+++ run.py
@@ -4,7 +4,11 @@
 @time: 2017/10/3 14:10
 """
 
+import sys
 from app.app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if len(sys.argv) < 2:
+        exit(0)
+    port = int(sys.argv[1])
+    app.run(debug=True, port=port)
